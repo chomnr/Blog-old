070c529eb4de19a969e18e221f066bb86d52f8c1
okjlez <zeljkovranjes@outlook.com>
2023-03-28 22:18:00 -0400

    added simple session_create inside user_service.rs

--- packages/server/blog/src/api/routes/user_routes.rs
+++ packages/server/blog/src/api/routes/user_routes.rs
@@ -10,6 +10,13 @@ struct CreateUser {
     email: String,
 }
 
+#[derive(serde::Serialize, serde::Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct LoginUser {
+    login: String,
+    password: String,
+}
+
 // login method... with cookie...
 #[post("/create", format = "json", data = "<post_data>")]
 async fn create_account(post_data: Json<CreateUser>, user: &State<Mutex<Service<User>>>) -> Result<Redirect, ()> {
@@ -21,6 +28,16 @@ async fn create_account(post_data: Json<CreateUser>, user: &State<Mutex<Service<
     }
 }
 
+#[post("/login", format = "json", data = "<post_data>")]
+async fn login_account(post_data: Json<LoginUser>, user: &State<Mutex<Service<User>>>) -> Result<Redirect, ()> {
+    //let create = user.lock().await.login(login, password)
+    // add messages...
+    match create {
+        Ok(v) => Ok(Redirect::to("/")), //todo
+        Err(_) => Err(())
+    }
+}
+
 pub fn routes() -> Vec<Route> {
-    routes![create_account]
+    routes![create_account, login_account]
 }
