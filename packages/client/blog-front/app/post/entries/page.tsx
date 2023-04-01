060726ac94610b3504cc2c68e5727a56345ee619
okjlez <zeljkovranjes@outlook.com>
2023-04-01 18:26:22 -0400

    added entries to the frontend..

--- packages/client/blog-front/app/post/entries/page.tsx
+++ packages/client/blog-front/app/post/entries/page.tsx
@@ -1,15 +1,45 @@
 'use client';
 
 import { Metadata } from 'next'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Navbar  from "@/components/navbar"
 
 
 export default function Page() {
+  let url = "http://localhost:8000/api/post/entries"
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <>
     <Navbar/>
+    
+    <div className='flex flex-col items-center bg-white h-fit pb-8 '>
+    {posts.map((post) => (
+
+      <div id={`blog-${post["blog_id"]}`} key={`blog-${post["blog_id"]}`} className='flex flex-col min-w-[500px] mt-3 pl-3 text-black hover:bg-[#F2F2F2] cursor-pointer'>
+        <div className='flex flex-col p-1'>
+            {post["author"]}
+            <div className='flex flex-row font-bold'>
+            {post["title"]}
+            </div>
+            {new Date(post["created_on"]).toDateString()}
+        </div>
+      </div>
+      ))}
+    </div>
+    </>
+  )
+}
+
+/*
+
     <div className='flex flex-col items-center bg-white h-fit pb-8 '>
       sadsddassd
       <div id="blog-post-1" className='flex flex-col min-w-[500px] mt-3 pl-3 border-l-2 border-t-2 border-[#4E192B] text-black hover:bg-[#F2F2F2] cursor-pointer'>
@@ -71,6 +101,4 @@ export default function Page() {
       </div>
 
     </div>
-    </>
-  )
-}
+    */
